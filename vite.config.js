033e86fcb5ff62b7c9e577ef37a3a04e8e867c65
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2025-05-11 16:07:33 +0300

    Initialize project with Vite, create gallery and form HTML pages, move existing styles and implement image gallery functionality

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url)) + '/src';
+console.log(__dirname);
+export default defineConfig({
+  root: __dirname,
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+});
