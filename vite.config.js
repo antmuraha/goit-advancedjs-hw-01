1be800af778497ec78d1ee8745080773835e7c90
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2025-05-20 01:40:46 +0300

    fix github pages: enhance output configuration for build process in Vite

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,25 @@ export default defineConfig(({ command, mode }) => ({
         '1-gallery': resolve(__dirname, '1-gallery.html'),
         '2-form': resolve(__dirname, '2-form.html'),
       },
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        entryFileNames: chunkInfo => {
+          if (chunkInfo.name === 'commonHelpers') {
+            return 'commonHelpers.js';
+          }
+          return '[name].js';
+        },
+        assetFileNames: assetInfo => {
+          if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+            return '[name].[ext]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
     },
   },
 }));
